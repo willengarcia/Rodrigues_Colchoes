59bfcbb2d714649c359f1700d8938db7c4d51583
Willen Garcia <willen.garcia05@gmail.com>
2025-03-25 17:21:13 -0300

    final

--- chamados/BotTelegramGLPI/src/admin/admin.ts
+++
@@ -1,7 +0,0 @@
-import { Telegraf, Context } from 'telegraf';
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-const api = process.env.TOKEN_API || "";
-const bot = new Telegraf(api);
-
